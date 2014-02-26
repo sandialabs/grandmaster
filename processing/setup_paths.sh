3fd3c466a01bf99768dc800dabae82fe94fc9a2c
Nathan Fabian <ndfabia@sandia.gov>
2014-02-26 15:40:58 -0700

    script to establish the titan paths necessary for processing.

---
+++ processing/setup_paths.sh
@@ -0,0 +1,3 @@
+export DYLD_LIBRARY_PATH=$PWD/../titan/titan-2.0-build/lib:$PWD/../titan/titan-2.0-build/TPL/VTK/Wrapping/Python:$PWD/../titan/titan-2.0-build/TPL/Boost-Install/lib:$PWD/../titan/titan-2.0-build/TPL/VTK/lib:${DYLD_LIBRARY_PATH}
+
+export PYTHONPATH=$PWD/../titan/titan-2.0-build/lib:$PWD/../titan/titan-2.0-build/TPL/VTK/Wrapping/Python:$PWD/../titan/titan-2.0-build/TPL/VTK/lib:${PYTHONPATH}
